4e707e585c813d050e17afd95f689e2a6c24b084
burhansenkkal <99999130+burhansenkkal@users.noreply.github.com>
2022-02-19 03:02:52 +0300

    FINAL PROJECT

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-function Calculate()
-    {
-        let p=document.getElementById('amount').value;
-        let t=document.getElementById('time').value;
-        let r=document.getElementById('rate').value;
+function compute(){
+    var principal = document.getElementById("principal").value;
 
-        let  SI=(p*t*r)/100;
+}
+function compute(){
+    var rate = document.getElementById("rate").value;
 
-        document.getElementById('si').innerHTML="The Total Simple Intrest Is: "+SI;
-    }
+}
+function compute(){
+    var years = document.getElementById("years").value;
+}
+function compute(){
+    var interest = principal * years * rate /100;
+}
+function compute(){
+    var year = new Date().getFullYear()+parseInt(years);
+}
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
